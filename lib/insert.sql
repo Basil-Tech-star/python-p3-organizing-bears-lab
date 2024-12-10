a7db36482bf7c9079aa55249a9e129eca8802237
Basil Wabuke <babazwabuke4@gmail.com>
2024-12-10 21:43:22 +0300

    adding changes to .sql files

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO  bears(id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 30, "M", "Black", "Soft", 1),
+(2,"Rowdy", 15, "M", "Brown","Moody", 0 ),
+(3,"Tabitha", 23, "F", "Brown", "Kind", 1),
+(4,"Sergeant Brown", 40, "M", "Black", "Adaptable", 1),
+(5,"Melissa", 35, "F", "Black", "Kind", 1),
+(6,"Grinch", 28, "M", "Brown","Sensitive", 1 ),
+(7,"Wendy", 24, "F", "Black", "Grumpy", 0),
+(8,NULL, 30, "F", "Brown", "Knd", 0);
